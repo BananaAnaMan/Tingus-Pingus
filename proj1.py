05aafc5ac4fd3f4c892d972f5a6aee1c10aac25e
roskar1 <ryan.oskar06@gmail.com>
2024-11-16 14:59:22 -0600

    Update proj1.py

--- proj1.py
+++ proj1.py
@@ -1,6 +1,27 @@
 import torch
+from torchvision import datasets
+from torchvision.transforms import ToTensor, Lambda
+
+#torch.utils.data.DataLoader
+
+#torch.utils.data.Dataset
+
+ds = datasets.FashionMNIST(
+    root="data",
+    train=True,
+    download=True,
+    transform=ToTensor(),
+    target_transform=lambda y: torch.zeroes(10, dtype=torch.float).scatter_(0, torch.tensor(y), value=1)
+)
+
+
 
 
 x = torch.rand(5, 3)
 print(x)
 print("hello World")
+
+#Get a data set
+#Analize the Data set
+#program model to predict stuff about new input
+#Develop front end website
